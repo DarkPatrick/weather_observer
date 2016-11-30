f00ef986ffac0afc407f4219c1df69ed6099e26b
DarkPatrick <egorsiomin@yandex.ru>
2016-11-30 11:26:27 +0300

    fully operational temperature graph

--- chart.js
+++ chart.js
@@ -13,11 +13,13 @@ class DateTime {
       this.hour = parseInt(arguments[3]);
       this.minute = parseInt(arguments[4]);
     }
-    var leap_years = (this.year - 1) / 4 - (this.year - 1) / 100 + (this.year - 1) / 400;
+    var leap_years = (this.year - 1) / 4 -
+      (this.year - 1) / 100 + (this.year - 1) / 400;
     var non_leap_years = this.year - leap_years;
     var m_in_y = leap_years * 527040 + non_leap_years * 525600;
     var m_in_m = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-    if (((this.year % 4 == 0) && (this.year % 100 != 0)) || (this.year % 400 == 0)) {
+    if (((this.year % 4 == 0) &&
+        (this.year % 100 != 0)) || (this.year % 400 == 0)) {
       m_in_m[1] = 29;
     }
     for (var i = 0; i < this.month - 1; i++) {
@@ -25,6 +27,14 @@ class DateTime {
     }
     m_in_y += (this.day - 1) * 1440 + this.hour * 60 + this.minute;
     this.ful_val = m_in_y;
+  }
+
+  printDateTime() {
+    return (this.day +
+            "/" + this.month +
+            "/" + this.year +
+            " #" + this.hour + 
+            ":" + this.minute);
   }
 }
 
@@ -161,18 +171,38 @@ function get_data() {
   }
 };
 
+var window_width;
+var window_height;
+
+var data_arr;
+var min_date;
+var max_date;
+
+var total_num;
+var good_dates;
+var arr_idx;
+var last_date;
+var fist_date;
+var max_val;
+var min_val;
+
+var re_points;
+
 function doAllStuff(file_data) {
-  var window_width = 800;
+  window_width = (document.body.clientWidth) * 0.95;
+  window_height = (document.body.clientHeight) / 2.1;
 
-  var data_arr = new FullDataArray(file_data);
-  var min_date = new DateTime(05, 11, 2016, 20, 28);
-  var max_date = new DateTime(15, 11, 2016, 20, 37);
+  data_arr = new FullDataArray(file_data);
+  min_date = new DateTime(05, 11, 2016, 20, 28);
+  max_date = new DateTime(15, 11, 2016, 20, 37);
 
-  var total_num = 0;
-  var good_dates = new Array();
-  var arr_idx = new Array();
-  var last_date = 0;
-  var fist_date = 0;
+  total_num = 0;
+  good_dates = new Array();
+  arr_idx = new Array();
+  last_date = 0;
+  fist_date = 0;
+  max_val = -1;
+  min_val = 300;
 
   for (var i = 0; i < data_arr.nums; i++) {
     if ((data_arr.date_time[i].ful_val >= min_date.ful_val) &&
@@ -185,6 +215,12 @@ function doAllStuff(file_data) {
       }
       last_date = data_arr.date_time[i].ful_val;
       arr_idx[total_num] = i;
+      if (data_arr.temperature[i] < min_val) {
+        min_val = data_arr.temperature[i];
+      }
+      if (data_arr.temperature[i] > max_val) {
+        max_val = data_arr.temperature[i];
+      }
       total_num++;
     }
   }
@@ -194,22 +230,104 @@ function doAllStuff(file_data) {
     good_dates[i] = Math.round(good_dates[i] * step);
   }
 
-  var c = document.getElementById("temperature_chart_canvas");
-  var ctx = c.getContext("2d");
+  re_points = new Array(total_num);
+
+  for (var i = 0; i < total_num; ++i) {
+    re_points[i] = new Array(2);
+    re_points[i][0] = 0;
+    re_points[i][1] = 0;
+  }
+
+  var ctx = temperature_el.getContext("2d");
   ctx.strokeStyle="#FF0000";
   ctx.beginPath();
-  ctx.moveTo(good_dates[0], 200 - data_arr.temperature[arr_idx[0]]);
+  re_points[0][0] = good_dates[0];
+  re_points[0][1] = window_height - 12 -
+    (data_arr.temperature[arr_idx[0]] - min_val) *
+    (window_height - 12) / (max_val - min_val);
+  ctx.moveTo(re_points[0][0], re_points[0][1]);
+
   var cur_pos = 0;
+  
   for (var i = 1; i < total_num; i++) {
     cur_pos += good_dates[i];
-    ctx.lineTo(cur_pos, 200 - data_arr.temperature[arr_idx[i]]);
+    re_points[i][0] = cur_pos;
+    re_points[i][1] = window_height - 12 -
+      (data_arr.temperature[arr_idx[i]] - min_val) *
+      (window_height - 12) / (max_val - min_val);
+    ctx.lineTo(re_points[i][0], re_points[i][1]);
   }
+  ctx.font="12px Times New Romans";
+  ctx.fillText(
+               data_arr.date_time[arr_idx[0]].printDateTime(),
+               0,
+               window_height - 2);
+  ctx.fillText(
+               data_arr.date_time[arr_idx[total_num - 1]].printDateTime(),
+               window_width - 90,
+               window_height - 2);
+  /*
+  todo:
+    графу-календарь для выбора отрезка дат для отображения
+  */
+  
+  ctx.stroke();
+}
+
+function getTemperaturePoints(event) {
+  var x = event.clientX;
+  var y = event.clientY;
+  var dist, min_dist;
+  var min_dist_n;
+  var ctx = temperature_el.getContext("2d");
+
+  ctx.clearRect(0, 0, window_width, window_height);
+  min_dist = Math.pow(x - re_points[0][0], 2) +
+    Math.pow(y - re_points[0][1], 2);
+  min_dist_n = 0;
+  ctx.strokeStyle="#FF0000";
+  ctx.beginPath();
+  ctx.moveTo(re_points[0][0], re_points[0][1]);
+  for (var i = 1; i < total_num; ++i) {
+    ctx.lineTo(re_points[i][0], re_points[i][1]);
+    dist = Math.pow(x - re_points[i][0], 2) +
+      Math.pow(y - re_points[i][1], 2);
+    if (dist < min_dist) {
+      min_dist = dist;
+      min_dist_n = i;
+    }
+  }
+  ctx.stroke();
+  ctx.strokeStyle="#00FF00";
+  ctx.beginPath();
+  ctx.moveTo(0, re_points[min_dist_n][1]);
+  ctx.lineTo(window_width, re_points[min_dist_n][1]);
+  ctx.moveTo(re_points[min_dist_n][0], 0);
+  ctx.lineTo(re_points[min_dist_n][0], window_height);
+  ctx.font="12px Times New Romans";
+  ctx.fillText(
+               data_arr.date_time[arr_idx[0]].printDateTime(),
+               0,
+               window_height - 2);
+  ctx.fillText(
+               data_arr.date_time[arr_idx[total_num - 1]].printDateTime(),
+               window_width - 90,
+               window_height - 2);
+  ctx.fillText(
+               data_arr.date_time[arr_idx[min_dist_n]].printDateTime() + "; " +
+               data_arr.temperature[arr_idx[min_dist_n]] + "°C",
+               window_width / 2,
+               window_height - 2);
   ctx.stroke();
 }
 
 
 var xhr;
+var temperature_el, pressure_el;
 var old_response = "";
+
+temperature_el = document.getElementById("temperature_chart_canvas");
+temperature_el.onmousemove = getTemperaturePoints;
 
 
 get_data();
